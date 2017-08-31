4e6861045325cc7495af6f2a9e52ae95d8b8b562
Camille Hall <camille18canlas@gmail.com>
2017-08-31 16:56:32 -0600

    unity

---
+++ UnityDGM1600/Assets/Script/camille.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camille : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("My name is camille");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
